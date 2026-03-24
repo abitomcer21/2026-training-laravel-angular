2105f43368b70c5a3aa9279faffa1fa1ed82ef40
Abigail Tomás <abigail.tomcer@gmail.com>
2026-03-24 17:14:38 +0100

    creando migracion order_lines

---
+++ backend/database/migrations/2026_03_24_161313_create_order_lines_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('order_lines', function (Blueprint $table) {
+            $table->id();
+            $table->uuid('uuid')->nullable()->unique();
+            $table->foreign('restaurante_id')->references('id')->on('restaurants');
+            $table->bigInteger('order_id')->references('id')->on('orders');
+            $table->bigInteger('product_id')->references('id')->on('products');
+            $table->bigInteger('user_id')->references('id')->on('users');
+            $table->integer('quantity');
+            $table->integer('price');
+            $table->integer('tax_percentage');
+            $table->timestamps();
+            $table->softDeletes('deleted_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('order_lines');
+    }
+};
