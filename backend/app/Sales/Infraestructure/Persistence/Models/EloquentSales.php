09397aeaa6e4dfffb884673c2e9ebfdfcfaff0f8
Abigail Tomás <abigail.tomcer@gmail.com>
2026-03-25 15:49:06 +0100

    Eliminación de archivos no necesarios

---
+++ backend/app/Sales/Infraestructure/Persistence/Models/EloquentSales.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Sales\Infraestructure\Persistence\Models;
+
+use Database\Factories\SalesFactory;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class EloquentSales extends Model
+{
+    use HasFactory;
+    use SoftDeletes;
+
+    protected $table = 'sales';
+
+    protected $fillable = [
+        'uuid',
+        'restaurant_id',
+        'order_id',
+        'user_id',
+        'ticket_number',
+        'value_date',
+        'total',
+    ];
+
+    public function getKeyName(): string
+    {
+        return 'id';
+    }
+
+    protected static function newFactory(): SalesFactory
+    {
+        return SalesFactory::new();
+    }
+
+    protected function casts(): array
+    {
+        return [
+            'value_date' => 'datetime',
+        ];
+    }
+}
+
